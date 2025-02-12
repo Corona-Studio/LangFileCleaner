b60d8224989bfaad041d9b154750723480a154cc
laolarou <luolinrui726@outlook.com>
2025-02-11 20:58:43 -0800

    add repair command

--- LangFileCleaner/Commands/RepairUnusedKeyCommand.cs
+++ LangFileCleaner/Commands/RepairUnusedKeyCommand.cs
@@ -1,4 +1,5 @@
-﻿using LangFileCleaner.Helpers;
+﻿using System.Collections.Frozen;
+using LangFileCleaner.Helpers;
 using System.CommandLine;
 using System.CommandLine.Invocation;
 using Serilog.Core;
@@ -9,7 +10,7 @@ public static class RepairUnusedKeyCommand
 {
     public static Command GetCommand()
     {
-        var unusedCommand = new Command("unused", "Check if there are unused keys in the Lang file.")
+        var unusedCommand = new Command("repair", "Repair Lang file by commenting all the unused Lang key.")
         {
             CommandConstants.UnusedKeys.RootPathOption,
             CommandConstants.UnusedKeys.LangFilePathOption,
@@ -40,9 +41,10 @@ public static class RepairUnusedKeyCommand
         await RepairLangFileAsync(root, srcFilePath, outFilePath, logger);
     }
 
-    public static async Task RepairLangFileAsync(string root, string srcFilePath, string outFilePath, Logger? logger)
+    private static async Task RepairLangFileAsync(string root, string srcFilePath, string outFilePath, Logger? logger)
     {
         root = $"{Path.GetFullPath(root)}{Path.DirectorySeparatorChar}";
+        outFilePath = Path.GetFullPath(outFilePath);
 
         var langFilePath = Path.Combine(root, srcFilePath);
         langFilePath = Path.GetFullPath(langFilePath);
@@ -51,5 +53,71 @@ public static class RepairUnusedKeyCommand
 
         var langFileContents = await File.ReadAllLinesAsync(langFilePath);
         var unusedKeys = await UnusedKeyCommand.ResolveAndCheckAsync(root, srcFilePath, logger);
+        var unusedKeyPatterns = unusedKeys.Select(k => $"x:Key=\"{k}\"").ToFrozenSet();
+        var isInCommentRange = false;
+        var isInMultipleLineResourceRange = false;
+
+        for (var lineNum = 0; lineNum < langFileContents.Length; lineNum++)
+        {
+            var line = langFileContents[lineNum];
+
+            if (string.IsNullOrWhiteSpace(line)) continue;
+
+            var trimmedLine = line.Trim();
+
+            // jump out the comment range
+            if (isInCommentRange && trimmedLine.EndsWith("-->", StringComparison.Ordinal))
+            {
+                isInCommentRange = false;
+                continue;
+            }
+
+            // entering multi-line comment
+            if (trimmedLine.StartsWith("<!--", StringComparison.Ordinal))
+            {
+                // skip comment line(s)
+                if (!trimmedLine.EndsWith("-->", StringComparison.Ordinal))
+                {
+                    // this is a multi-line comment
+                    isInCommentRange = true;
+                    continue;
+                }
+            }
+
+            var paddingStr = line[..^trimmedLine.Length];
+
+            // jump out the multi-line resource range
+            if (isInMultipleLineResourceRange && trimmedLine.EndsWith("</sys:String>", StringComparison.Ordinal))
+            {
+                langFileContents[lineNum] = $"{paddingStr}{trimmedLine} -->";
+                isInMultipleLineResourceRange = false;
+                continue;
+            }
+
+            // we skip this line if it is in comment range
+            if (isInCommentRange) continue;
+
+            var isMatched = unusedKeyPatterns.Any(p => line.Contains(p, StringComparison.OrdinalIgnoreCase));
+
+            if (!isMatched) continue;
+
+            // this resource is no longer used, so we add a comment to it
+            var isOneLiner = trimmedLine.EndsWith("</sys:String>", StringComparison.Ordinal);
+
+            logger?.Debug("Found unused {Oneliner} key: {Line}", line, isOneLiner ? "one-liner" : string.Empty);
+
+            if (isOneLiner)
+            {
+                langFileContents[lineNum] = $"{paddingStr}<!-- {trimmedLine} -->";
+                continue;
+            }
+
+            // this is a multi-line resource
+            langFileContents[lineNum] = $"{paddingStr}<!-- {trimmedLine}";
+            isInMultipleLineResourceRange = true;
+        }
+
+        logger?.Information("Writing repaired lang file to {OutFilePath}", outFilePath);
+        await File.WriteAllLinesAsync(outFilePath, langFileContents);
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # LangFileCleaner
+
+A tool written in C# used to help translators fix their lang doc quickly and smoothly
+
+## Usage
+
+### Check Lang file
+
+This command will check if a Lang file has any unused resource key.
+
+```bash
+./LangFileCleaner.exe unused -r "[PROJ_ROOT]" -f "[LANG_FILE_RELATIVE_PATH]" -fu -v
+```
+
+- `-fu`: When passing this flag, program will return non-zero exit code if there are any unused keys detected in the Lang file.
+- `-v`: When passing this flag, console will write the DEBUG logs.
+
+### Repair Lang file
+
+THis command will first scan unused keys in the Lang file and comment them out, then it will write the new result to the destination.
+
+```bash
+./LangFileCleaner.exe repair -r "[PROJ_ROOT]" -f "[LANG_FILE_RELATIVE_PATH]" -o "[OUT_PATH]" -v
+```
+
+- `-v`: When passing this flag, console will write the DEBUG logs.
